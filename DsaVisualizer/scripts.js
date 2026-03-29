416bb4ffac95297351aa7281f7053151ffce85b9
Nida <nidahafeez31@gmail.com>
2026-03-29 13:58:46 +0500

    Add Selection Sort with animation and UI improvements

--- DsaVisualizer/scripts.js
+++ DsaVisualizer/scripts.js
@@ -15,6 +15,8 @@ function generateArray(){
 }
 
 function bubbleSort(){
+    document.getElementById("algo-btn").innerText = "Bubble Sort";
+    document.getElementById("algo-desc").innerText = "The bubble sort works, by checking two adjacent elements and then swaps them to their correct position";
     let n = arr.length;
     let steps = [];
 
@@ -33,22 +35,25 @@ function bubbleSort(){
 
     for (let k = 0; k < steps.length; k++) {
         //This says — "run this function after this many milliseconds". It does NOT pause the code, it just schedules something for later.
-
         // setTimeout(func, 0 * 300);   // runs at 0ms
         //setTimeout(func, 1 * 300);   // runs at 300ms
         //setTimeout(func, 2 * 300);   // runs at 600ms
         //All three are scheduled instantly, but they fire at different times — creating the animation effect!
-
         // This is the trickiest part. The problem is — by the time setTimeout fires, the loop is already done and k is at its final value. So every timeout would use the same k!
         //The fix is to wrap it in an outer function that takes k as a parameter and immediately calls itself with (k). This freezes the current value of k for each timeout. Think of it like making a copy of k for each iteration.
         setTimeout(function(k) {
             return function() {
                 // This is called destructuring — it unpacks the array [j, j+1, true/false] into three separate variables in one line.
                 let [a, b, isSwap] = steps[k];
+                for (let x = 0; x < 8; x++) {
+                document.getElementById("bar" + x).style.backgroundColor = "rgb(131, 78, 115)";
+                }
+            // then highlight just the two
                 let barA = document.getElementById("bar" + a);
-                let barB = document.getElementById("bar" + b);
-                barA.style.backgroundColor = "grey";
-                barB.style.backgroundColor = "grey";
+                 let barB = document.getElementById("bar" + b);
+                    barA.style.backgroundColor = "DarkRed";
+                    barB.style.backgroundColor = "yellow";
+                
                 // Only swap visually if a swap actually happened in the algorithm.
                 if (isSwap) {
                     let temp = barA.style.height;
@@ -63,6 +68,66 @@ function bubbleSort(){
     // reset all bars to steelblue after sorting is done
     // reset all bars to steelblue after sorting is done
 setTimeout(function() {
+    
+    for (let i = 0; i < 8; i++) {
+        setTimeout(function(i) {
+            return function() {
+                document.getElementById("bar" + i).style.backgroundColor = "steelblue";
+                document.getElementById("message").innerText = "The data is now sorted";
+            }
+        }(i), i * 100);
+    }
+}, steps.length * 300 + 300);
+}
+
+function selectionSort(){
+    document.getElementById("algo-btn").innerText = "Selection Sort";
+    document.getElementById("algo-desc").innerText = "The selection sort works by finding the minimum element and placing it in the correct position ";
+    let n = arr.length;
+    let steps = [];
+
+    for (let i = 0; i < n-1; i++) {
+        let min = i;
+        for (let j = i+1; j < n; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+            steps.push([j, min, false]);
+        }
+                let temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+                steps.push([i, min, true]);
+                
+    }
+             // Instead of animating immediately, we first run the entire sort and save every single comparison into steps. Each step stores 3 things — left index, right index, and whether a swap happened.
+
+        // Animation   
+        for (let k = 0; k < steps.length; k++) {
+        setTimeout(function(k) {
+            return function() {
+                // This is called destructuring — it unpacks the array [j, j+1, true/false] into three separate variables in one line.
+                let [a, b, isSwap] = steps[k];
+                for (let x = 0; x < 8; x++) {
+                document.getElementById("bar" + x).style.backgroundColor = "rgb(131, 78, 115)";
+                }
+            // then highlight just the two
+                let barA = document.getElementById("bar" + a);
+                 let barB = document.getElementById("bar" + b);
+                    barA.style.backgroundColor = "red";
+                    barB.style.backgroundColor = "yellow";
+                // Only swap visually if a swap actually happened in the algorithm.
+                if (isSwap) {
+                    let temp = barA.style.height;
+                    barA.style.height = barB.style.height;
+                    barB.style.height = temp;
+                    barB.style.backgroundColor = "Yellow";
+                }
+            }
+        }(k), k * 300); // Waits until ALL steps are done (total time = steps × 300ms), then resets all bars back to steelblue.
+    }  
+        // Green Animation 
+    setTimeout(function() {
     for (let i = 0; i < 8; i++) {
         setTimeout(function(i) {
             return function() {
